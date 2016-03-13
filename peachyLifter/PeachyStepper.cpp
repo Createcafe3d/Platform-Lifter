428d28b0f0fbccbb02893c869d03175dbe453072
Harakan <wgt538@gmail.com>
2016-03-13 16:34:19 -0600

    Working stepper with rough tests through Makefile

---
+++ peachyLifter/PeachyStepper.cpp
@@ -0,0 +1,109 @@
+#include <stdint.h>
+
+#ifdef _TEST_PEACHYSTEPPER
+
+void digitalWrite(unsigned char,unsigned char){} //Stub it out for testing
+
+#include "PeachyStepper.h"
+
+#include <stdio.h>
+
+#define BYTETOBINARYPATTERN "%d%d%d%d%d%d%d%d"
+#define BYTETOBINARY(byte)  \
+	(byte & 0x80 ? 1 : 0), \
+  (byte & 0x40 ? 1 : 0), \
+  (byte & 0x20 ? 1 : 0), \
+  (byte & 0x10 ? 1 : 0), \
+  (byte & 0x08 ? 1 : 0), \
+  (byte & 0x04 ? 1 : 0), \
+  (byte & 0x02 ? 1 : 0), \
+  (byte & 0x01 ? 1 : 0) 
+
+void print_status(PeachyStepper *Stepper){
+	uint8_t stepper_state;
+
+	stepper_state = Stepper->getState();
+	printf("Stepper_state: "BYTETOBINARYPATTERN"\n", BYTETOBINARY(stepper_state));
+}
+
+void print_positions(PeachyStepper *Stepper){
+	int32_t stepper_position;
+	int32_t stepper_c_position;
+	uint8_t stepper_direction;	
+
+	stepper_c_position=Stepper->getCommandedPosition();
+	stepper_direction=Stepper->getDirection();
+	stepper_position=Stepper->getPosition();
+	printf("Stepper_position: %i\n",stepper_position);
+	printf("Stepper_c_position: %i\n",stepper_c_position);
+	printf("Stepper_direction: %d\n",stepper_direction);
+}
+
+void test_basicSteps(PeachyStepper *Stepper){
+	print_status(Stepper);
+	printf("\n STEPPING 1 times \n");
+	Stepper->move(STEPPER_UP);
+	Stepper->step();
+
+	print_status(Stepper);
+
+	printf("\n STEPPING 12 times \n");
+	for (int i=0;i<12;i++){
+		Stepper->move(STEPPER_UP);
+		Stepper->step();
+		print_status(Stepper);
+	}
+}
+
+void test_move(PeachyStepper *Stepper){
+	print_status(Stepper);
+	print_positions(Stepper);
+	printf("\n Moving 12 steps DOWN \n");
+	Stepper->move(STEPPER_DOWN,12);
+	for (int i=0;i<12;i++){
+		Stepper->step();
+		print_status(Stepper);
+		print_positions(Stepper);
+	}
+}
+
+void test_stop(PeachyStepper *Stepper){
+	printf("\n Moving 5 steps DOWN and stopping \n");
+	print_status(Stepper);
+	print_positions(Stepper);
+	Stepper->move(STEPPER_DOWN,12);
+	for (int i=0;i<5;i++){
+		Stepper->step();
+		print_status(Stepper);
+		print_positions(Stepper);
+	}
+	Stepper->stop();
+	print_status(Stepper);
+	print_positions(Stepper);
+}
+
+int main(){
+
+	PeachyStepper MyStepper;
+	print_status(&MyStepper);
+
+	test_basicSteps(&MyStepper);
+	test_move(&MyStepper);
+	test_stop(&MyStepper);
+	//test_move_zero();
+
+	
+	return 0;
+}
+
+
+
+
+
+#endif
+
+
+
+
+
+
--- peachyLifter/PeachyStepper.h
+++ peachyLifter/PeachyStepper.h
@@ -3,11 +3,18 @@
 #ifndef _PEACHYSTEPPER_H_
 #define _PEACHYSTEPPER_H_
 
+//The digital pins used for the stepper driver
 #define STEPPER_PIN0 8
 #define STEPPER_PIN1 9
 #define STEPPER_PIN2 10
 #define STEPPER_PIN3 11
 
+// How many times step gets called (minus one) between actual motor steps
+// These steps include the 0th step,
+// ie: 3 means 0->1->2->3 (4 calls)
+#define STEPPER_U_STEPS 3 //Must be greater than 0
+
+//These shouldn't need to be changed
 #define STEPPER_STABLE 2
 #define STEPPER_UP 1
 #define STEPPER_DOWN 0
@@ -24,12 +31,17 @@
 class PeachyStepper
 {
 	public:
-		PeachyStepper(){
+		PeachyStepper(uint8_t hold_torque = STEPPER_U_STEPS){
 			m_current_position=0;
 			m_commanded_position=0;
-			m_direction = STEPPER_UP;
-			m_hold_torque = 0; 
-			m_step_state=7;//[00000111]
+			m_direction = STEPPER_STABLE;
+			m_hold_torque = hold_torque; 
+			m_step_state = 7;//[00000111]
+			m_microstep_counter = 0;
+		}
+
+		void moveTo(uint32_t position){
+			m_commanded_position=position;
 		}
 
 		void move(uint8_t direction,uint32_t steps){
@@ -49,32 +61,86 @@ class PeachyStepper
 		}
 
 		void step(){
-			//Function called by the timer interrupt 
 			if (m_current_position == m_commanded_position){
-				//Switch to holding torque mode
-				//which for now means doing nothing
-				m_direction=STEPPER_STABLE
+				m_direction=STEPPER_STABLE;
 			}
-			else{
-				if (m_current_position > m_commanded_position)
+			else {
+				if (m_current_position < m_commanded_position){
 					m_direction = STEPPER_UP;
 					m_current_position++;
-				else // <
+				}
+				else{ // <
 					m_direction = STEPPER_DOWN;
 					m_current_position--;
+				}
 				shift_step();
 				assign_bits();
 			}
+		}
 
-		void zero_position(direction){
+		//Function called by the timer interrupt 
+		void micro_step(){
+			m_microstep_counter=(m_microstep_counter%STEPPER_U_STEPS)+1; //0->STEPPER_U_STEPS inclusive
+			
+			if (m_current_position == m_commanded_position){
+				//Switch to holding torque mode
+				m_direction=STEPPER_STABLE;
 
+				//ie: hold_torque==2, counter==3, microsteps==0->3 -> pins off for that one cycle 
+				if (m_microstep_counter>m_hold_torque){ 
+					pins_off();
+				}
+				//ie: otherwise keep those pins on for the other 3 cycles (0,1,2)
+				else{ 
+					pins_on();
+				}
+				
+			}
+			else if (m_microstep_counter==0){
+				if (m_current_position < m_commanded_position){
+					m_direction = STEPPER_UP;
+					m_current_position++;
+				}
+				else{ // <
+					m_direction = STEPPER_DOWN;
+					m_current_position--;
+				}
 
+				shift_step();
+				assign_bits();
+			}
+		}
+
+		void stop(){
+			m_commanded_position=m_current_position;
+		}
+
+		void zeroPosition(){
+			m_current_position = 0;
+			m_commanded_position = 0;
+		}
+
+		int32_t getPosition(){
+			return m_current_position;
+		}
+
+		int32_t getCommandedPosition(){
+			return m_commanded_position;
+		}
+
+		uint8_t getState(){
+			return m_step_state;
+		}
+
+		uint8_t getDirection(){
+			return m_direction;
 		}
 
 	private:
 		uint8_t m_direction;
 		uint8_t m_hold_torque;
 		uint8_t m_step_state;
+		uint8_t m_microstep_counter;
 		int32_t m_commanded_position;
 		int32_t m_current_position;
 
@@ -82,22 +148,34 @@ class PeachyStepper
 			uint8_t carry_bit;
 
 			if (m_direction == STEPPER_UP){
-				carry_bit=m_step_state && MASK_TOPBIT;
+				carry_bit=m_step_state & MASK_TOPBIT;
 				m_step_state=m_step_state<<1; //Shift up
-				if (carry_bit)
+				if (carry_bit==0x80)
 					m_step_state+=0x01;//Bring in the carry
 			}
 			else if(m_direction == STEPPER_DOWN){
-				carry_bit=m_step_state && MASK_BOTBIT;
+				carry_bit=m_step_state & MASK_BOTBIT;
 				m_step_state=m_step_state>>1; //Shift down
-				if (carry_bit)
+				if (carry_bit==0x01)
 					m_step_state+=0x80;//Add in the carry at the top
 			}
+		}
+
+		void pins_off(){
+			digitalWrite(STEPPER_PIN0,0);
+			digitalWrite(STEPPER_PIN1,0);
+			digitalWrite(STEPPER_PIN2,0);
+			digitalWrite(STEPPER_PIN3,0);
+		}
+
+		void pins_on(){
+			assign_bits();
 		}
 
 		void assign_bits()
 		{
 			// 4 pin servos "on" steps:
+			// [pin0 pin1 pin2 pin3]
 			// [1 0 0 0] 0 
 			// [1 1 0 0] 1 
 			// [0 1 0 0] 2 
@@ -108,17 +186,17 @@ class PeachyStepper
 			// [1 0 0 1] 7 
 			uint8_t tmp_bit;
 
-			tmp_bit = |(m_step_state && MASK_PIN0);
+			tmp_bit = (m_step_state & MASK_PIN0);
 			digitalWrite(STEPPER_PIN0,tmp_bit);
 
-			tmp_bit = |(m_step_state && MASK_PIN1);
+			tmp_bit = (m_step_state & MASK_PIN1);
 			digitalWrite(STEPPER_PIN1,tmp_bit);
-			
-			tmp_bit = |(m_step_state && MASK_PIN2);
+
+			tmp_bit = (m_step_state & MASK_PIN2);
 			digitalWrite(STEPPER_PIN2,tmp_bit);
 
-			tmp_bit = |(m_step_state && MASK_PIN3);
+			tmp_bit = (m_step_state & MASK_PIN3);
 			digitalWrite(STEPPER_PIN3,tmp_bit);
 		}
-
+};
 #endif
